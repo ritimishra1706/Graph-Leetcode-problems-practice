24400228c74ced98a6b39364ceefa61162da8dea
Ritisha Mishra <89974292+ritimishra1706@users.noreply.github.com>
2024-04-12 10:45:48 +0530

    Time: 252 ms (87.04%), Space: 17.4 MB (32.61%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,20 +1,25 @@
 class Solution:
+    # USING BFS
     def numSquares(self, n: int) -> int:
-        k=int(math.sqrt(n))
-        # column is sum, row is square roots
-        dp=[[0 for i in range(n+1)]for j in range(k)]
-        for i in range(n+1):
-            dp[0][i]=i
-        for i in range(1,k):
-            for j in range(1,n+1):
-                sq=(i+1)*(i+1)
-                if sq>j:
-                    dp[i][j]=dp[i-1][j]
-                else:
-                    # min(previous count if not using current perfect square,
-                    # (j//sq)--> how many times we can use current perfect square
-                    # [j%sq]--> which perfect square need for remaining sum)
-                    dp[i][j]=min(dp[i-1][j],(j//sq)+dp[i][j%sq])
-        return dp[-1][-1]                    
+        q=[]
+        # sum and count
+        q.append([0,0])
+        visit=[0]*(n+1)
+        visit[0]=1
+        
+        while q:
+            s,count=q.pop(0) 
+            for i in range(1,int(math.sqrt(n))+1):
+                if s+i*i==n:
+                    return count+1
+                s1=s+i*i    
+                if s1<n and visit[s1]==0:
+                    c=count+1
+                    q.append([s1,c])
+                    visit[s1]=1
+                    
+    
 
+
+            
         
